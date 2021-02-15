182b98309eec4dddc1e5e4e4f383566a78c266ce
JamesQ02 <77318371+JamesQ02@users.noreply.github.com>
2021-02-15 21:03:12 +0800

    Add files via upload

---
+++ app/src/main/java/mcm/edu/ph/qiu_calculator/MainActivity.java
@@ -0,0 +1,32 @@
+package mcm.edu.ph.qiu_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button,button2,button3,button4,button5,button6,button7,button8,button9,button10,button12,button11,button13,button15,btnMod;
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
+        button6 = findViewById(R.id.button6);
+        button7 = findViewById(R.id.button7);
+        button8 = findViewById(R.id.button8);
+        button9 = findViewById(R.id.button9);
+        button10 = findViewById(R.id.button10);
+
+
+    }
+
+
+}
